47d795512cd74b36ec58f671a5c890da839e7487
ahza64 <justinmichaelpettit@gmail.com>
2022-04-16 21:06:53 -0700

    context complete and displaying favorites

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,8 +1,16 @@
+import { useContext } from 'react'
+import MeetupList from '../components/meetups/MeetupList'
+
+import FavoritesContext from '../store/favorites-context'
+
 function FavoritesPage() {
+  const favoritesCtx = useContext(FavoritesContext)
+
   return (
-    <div>
-      Favorites
-    </div>
+    <section>
+      <h1>My Favorites</h1>
+      <MeetupList meetups={favoritesCtx.favorites}/>
+    </section>
   )
 }
 
