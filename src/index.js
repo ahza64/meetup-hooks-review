42be145a944f30d6bf82c08f5a0f4089b1480529
ahza64 <justinmichaelpettit@gmail.com>
2022-04-17 18:18:54 -0700

    update to react 18

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import { FavoritesContextProvider } from './store/favorites-context'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <FavoritesContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </FavoritesContextProvider>,
-  document.getElementById('root')
+  </FavoritesContextProvider>
 );
