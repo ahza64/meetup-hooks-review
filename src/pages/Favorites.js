5e999efe5b2e067ce8bd74e390c2ec27e4055c8e
ahza64 <justinmichaelpettit@gmail.com>
2022-04-17 18:05:27 -0700

    favorites badge with useContext

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,15 +1,23 @@
 import { useContext } from 'react'
-import MeetupList from '../components/meetups/MeetupList'
 
+import MeetupList from '../components/meetups/MeetupList'
 import FavoritesContext from '../store/favorites-context'
 
 function FavoritesPage() {
   const favoritesCtx = useContext(FavoritesContext)
 
+  let content
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You don't have any favorites yet</p>
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites}/>
+  }
+
   return (
     <section>
       <h1>My Favorites</h1>
-      <MeetupList meetups={favoritesCtx.favorites}/>
+      {content}
     </section>
   )
 }
